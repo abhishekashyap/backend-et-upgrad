e93f6cbf3da019cae179628a90e8dd59a21b820f
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2021-04-28 15:36:32 +0530

    Initial boilerplate

---
+++ src/main/java/com/backend/endterm/EndtermApplication.java
@@ -0,0 +1,13 @@
+package com.backend.endterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndtermApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndtermApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/endterm/EndtermApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.endterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndtermApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
