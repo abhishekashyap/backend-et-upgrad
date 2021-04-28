9c1af2e644bc4c3323c68c838580425bcb605f1b
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2021-04-28 15:53:16 +0530

    Created base controller

--- src/main/java/com/backend/endterm/controller/UserController.java
+++ src/main/java/com/backend/endterm/controller/UserController.java
@@ -1,4 +1,32 @@
 package com.backend.endterm.controller;
 
+import com.backend.endterm.model.User;
+import com.backend.endterm.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
 public class UserController {
+    private UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping("/addUser")
+    public User storeUser(@RequestBody User aUser) {
+        return userRepository.save(aUser);
+    }
+
+    @DeleteMapping("/deleteUser/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
 }
--- src/main/java/com/backend/endterm/repository/UserRepository.java
+++ src/main/java/com/backend/endterm/repository/UserRepository.java
@@ -1,4 +1,6 @@
 package com.backend.endterm.repository;
 
-public interface UserRepository {
-}
+import com.backend.endterm.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {}
